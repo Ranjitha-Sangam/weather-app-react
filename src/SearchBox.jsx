b1ab05a9414127edad01544fde80aa1ec6bb849d
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-05-14 08:43:01 +0530

    added environment variable setup

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}) {
   let [city, setCity] = useState("");
   let [error,setError] = useState(false);
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY ="b79e481d46bff121a9c8c8beecdaba7c";
+  const API_KEY =import.meta.env.VITE_WEATHER_API_KEY;
 
   let getWeatherInfo = async() =>{
     try{
